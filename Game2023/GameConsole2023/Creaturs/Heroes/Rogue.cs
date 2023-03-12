035fb8f3c8d3c418749fe56f0f02ddc826daebff
fr1ssst <ratyshnui2008@gmail.com>
2023-03-12 22:16:12 +0200

    Add folder

---
+++ Game2023/GameConsole2023/Creaturs/Heroes/Rogue.cs
@@ -0,0 +1,24 @@
+﻿using GameConsole2023.Creaturs;
+using System;
+
+namespace GameConsole2023.Creaturs.Heroes
+{
+    internal class Rogue : BasePersone
+    {
+        Random random = null;
+        public Rogue(string name) : base(name)
+        {
+            random = new Random();
+
+            this.classPlayer = "Розбійник";
+            this.health = 600;
+            this.energy = 95;
+            this.power = random.Next(54, 62);
+            this.agility = 325;
+            this.endurance = 30;
+            this.armor = 3;
+            this.level = 1;
+            this.numberOFEP = 339;
+        }
+    }
+}
