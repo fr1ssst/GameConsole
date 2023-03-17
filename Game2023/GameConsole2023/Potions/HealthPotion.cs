45da2818af9ff2e2119248b8a28e6223e579ec3d
fr1ssst <ratyshnui2008@gmail.com>
2023-03-17 12:44:08 +0200

    Added game folder.17

---
+++ Game2023/GameConsole2023/Potions/HealthPotion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GameConsole2023.Potions.PlayerPotion;
+using GameConsole2023.Creaturs;
+using GameConsole2023.GameInterface;
+
+namespace GameConsole2023.Potions
+{
+
+    class HealthPotion : PlayerPotion, IUsePotion
+    {       
+        int m_value;
+        public HealthPotion(int value) : base(value)
+        {
+            m_value = value;
+        }
+
+        public void UsePotion(BasePersone persone)
+        {
+            persone.heal(m_value);
+        }
+        public string getNamePotion() 
+        {
+            return "HealthPotion";
+        }
+    }
+}
---
+++ Game2023/GameConsole2023/Potions/PotionSeating.cs
@@ -0,0 +1,20 @@
+﻿using GameConsole2023.Creaturs;
+using GameConsole2023.Skills;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole2023.Potions
+{
+    abstract class PlayerPotion : NameTrait
+    {
+        protected int value = 0;
+        public PlayerPotion(int value)
+        {
+            this.value = value;
+        }
+
+    }
+}
